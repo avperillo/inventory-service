355d7317ae535b7d70d96c74e0dcad007f3a986d
Alex Vázquez <alexander.vazquez@bosonit.com>
2022-06-25 16:38:51 +0200

    add entityframework configuration

---
+++ src/Inventory.Infrastructure/EntityTypeConfiguration/ItemEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Domain.Aggregates.Items;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.Infrastructure.EntityTypeConfiguration
+{
+    class ItemEntityTypeConfiguration : IEntityTypeConfiguration<Item>
+    {
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder
+                .Property(b => b.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(b => b.ExpirationDate);
+            builder.Property(b => b.Type);
+
+        }
+    }
+}
---
+++ src/Inventory.Infrastructure/InventoryContext.cs
@@ -0,0 +1,29 @@
+﻿using DDD.abstracts;
+using Inventory.Domain.Aggregates.Items;
+using Inventory.Infrastructure.EntityTypeConfiguration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Inventory.Infrastructure
+{
+
+    public class InventoryContext : DbContext, IUnitOfWork
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ItemEntityTypeConfiguration());
+        }
+
+        public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
