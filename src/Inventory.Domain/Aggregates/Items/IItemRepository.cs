61a4c93b592323135e16a9da354be1175753074c
Alex Vázquez <alexander.vazquez@bosonit.com>
2022-06-25 15:23:31 +0200

    add project layers: Application, Infra and API

--- src/Inventory.Domain/Aggregates/Items/IItemRepository.cs
+++ src/Inventory.Domain/Aggregates/Items/IItemRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DDD.abstracts;
 
 namespace Inventory.Domain.Aggregates.Items
 {
-    interface IItemRepository
+    public interface IItemRepository: IRepository<Item>
     {
+
     }
 }
