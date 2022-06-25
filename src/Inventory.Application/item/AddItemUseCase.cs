56aa1c135a7a3f0b63791514470d3c1a28dde4fa
Alex Vázquez <alexander.vazquez@bosonit.com>
2022-06-25 17:12:41 +0200

    implement add in item repository

---
+++ src/Inventory.Application/item/AddItemUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.Domain.Aggregates.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Application.item
+{
+    public class AddItemUseCase
+    {
+        public AddItemUseCase(IItemRepository repo)
+        {
+
+        }
+    }
+}
